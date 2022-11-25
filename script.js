527ca76dbe76d144432e4523c08e06e713be25c7
Alisson Lins <alissonlinsbispo@gmail.com>
2022-11-25 16:08:13 -0300

    Detalhes Atualizados

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
-            $('.scroll-up-btn').removeClass("show");  
+            $('.scroll-up-btn').removeClass("show");
         }
     });
     $('.scroll-up-btn').click(function(){
-        $('html').animate({scrolltop: 0});
-    }); 
+        $('html').animate({scrollTop: 0});
+    });
 
     var typed = new Typed(".typing", {
         strings:["Desenvolvedor", "Estudante de CyberSecurity"],
